577a7cc4b85da5f3b52d3b4c0332dde079987523
bernardoazevedo <bernardoazevedo.8@gmail.com>
2024-07-31 20:39:47 -0300

    div to show links styled

--- resources/views/links/index.blade.php
+++ resources/views/links/index.blade.php
@@ -17,22 +17,28 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
-            <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 text-gray-800 dark:text-gray-200 shadow sm:rounded-lg">
-                @foreach ($links as $link)
-                    <div class="p-2 flex space-x-2">
-                        <div class="flex-1">
+            @foreach ($links as $link)
+                <div class="p-4 bg-white dark:bg-gray-800 text-gray-800 dark:text-gray-200 shadow sm:rounded-lg">
+                    <div class="flex flex-col space-y-3">
                             <x-nav-link href="{{ 'https://'.$link->url }}">
-                                <p class="mt-4 text-lg ">{{ $link->text }}</p>
+                                <p class="text-lg ">{{ $link->text }}</p>
                             </x-nav-link>
-                            <div class="flex justify-between items-center">
-                                <div>
-                                    <small class="text-sm ">Added at: {{ $link->created_at->format('j M Y, g:i a') }}</small>
+
+                            <div>
+                                <div class="flex items-center">
+                                    <p class="">Link:</p>
+                                    <p class="ml-2 text-lg">{{ $link->url }}</p>
+                                </div>
+                                <div class="flex items-center">
+                                    <p class="">Text:</p>
+                                    <p class="ml-2 text-lg">{{ $link->text }}</p>
                                 </div>
                             </div>
-                        </div>
+
+                            <small class="text-sm ">Added at: {{ $link->created_at->format('j M Y, g:i a') }}</small>
                     </div>
-                @endforeach
-            </div>
+                </div>
+            @endforeach
         </div>
     </div>
 
