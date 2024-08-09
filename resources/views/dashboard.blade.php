d2b5cb7eb62450ffc2845766b02d681e445db35b
bernardoazevedo <bernardoazevedo.8@gmail.com>
2024-08-09 20:04:41 -0300

    button to create link page

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,14 +7,7 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-4">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
-                </div>
-            </div>
-
-            <x-responsive-nav-link href="{{ url('/links') }}">Create my links</x-responsive-nav-link>
+            <a href="{{ route('links.index') }}"><x-primary-button>{{ __('Create my links') }}</x-primary-button></a>
         </div>
     </div>
-
 </x-app-layout>
