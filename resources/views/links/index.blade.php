0a0b346ac5e07027fad382793853ba540f369ea1
bernardoazevedo <bernardoazevedo.8@gmail.com>
2024-08-10 09:13:32 -0300

    button to view page moved to first section

--- resources/views/links/index.blade.php
+++ resources/views/links/index.blade.php
@@ -8,20 +8,21 @@
     <div class="py-6">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
             <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('links.partials.create-link-form')
+                <div class="max-w-xl text-gray-800 dark:text-gray-200 text-lg">
+                    <x-nav-link href="{{ env('APP_URL') .'/p/'. Auth::user()->username }}" target="_blank">
+                        <p class="text-lg ">View my page</p>
+                    </x-nav-link>
                 </div>
             </div>
         </div>
     </div>
 
+
     <div class="py-6">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
             <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
-                <div class="max-w-xl text-gray-800 dark:text-gray-200 text-lg">
-                    <x-nav-link href="{{ env('APP_URL') .'/p/'. Auth::user()->username }}" target="_blank">
-                        <p class="text-lg ">View my page</p>
-                    </x-nav-link>
+                <div class="max-w-xl">
+                    @include('links.partials.create-link-form')
                 </div>
             </div>
         </div>
