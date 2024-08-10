34a53aa4f637ce44f9252b28183aea036a8da5a3
bernardoazevedo <bernardoazevedo.8@gmail.com>
2024-08-10 09:11:47 -0300

    logo added

--- resources/views/links/index.blade.php
+++ resources/views/links/index.blade.php
@@ -19,7 +19,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
             <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
                 <div class="max-w-xl text-gray-800 dark:text-gray-200 text-lg">
-                    <x-nav-link href="{{ env('APP_URL') .':8000/p/'. Auth::user()->username }}" target="_blank">
+                    <x-nav-link href="{{ env('APP_URL') .'/p/'. Auth::user()->username }}" target="_blank">
                         <p class="text-lg ">View my page</p>
                     </x-nav-link>
                 </div>
