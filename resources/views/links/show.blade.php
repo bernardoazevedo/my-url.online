89535990ab296d3cca164ee2c851152793400f7e
bernardoazevedo <bernardoazevedo.8@gmail.com>
2024-08-10 10:19:29 -0300

    included header to show page

--- resources/views/links/show.blade.php
+++ resources/views/links/show.blade.php
@@ -1,50 +1,22 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ $username }} links
+        </h2>
+    </x-slot>
 
-        <title>{{ config('app.name', 'LinksOnline') }}</title>
-        <link rel="shortcut icon" href="{{ url('/logo.svg') }}" type="image/x-icon">
-
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
-
-            @include('layouts.navigation')
-
-            <!-- Page Heading -->
-            @isset($header)
-                <header class="bg-white dark:bg-gray-800 shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
+            @foreach ($userLinks as $link)
+                <div class="p-4 bg-white dark:bg-gray-800 text-gray-800 dark:text-gray-200 shadow sm:rounded-lg">
+                    <div class="flex flex-col space-y-3">
+                        <x-show-link href="{{ 'https://'.$link->url }}" target="_blank">
+                            <p class="text-lg ">{{ $link->text }}</p>
+                        </x-show-link>        
                     </div>
-                </header>
-            @endisset
-
-            <div class="py-12">
-                <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
-                    <h1 class="px-4 text-gray-800 dark:text-gray-200 text-xl font-medium">
-                        {{ $username }} links
-                    </h1>
-                    @foreach ($userLinks as $link)
-                        <div class="p-4 bg-white dark:bg-gray-800 text-gray-800 dark:text-gray-200 shadow sm:rounded-lg">
-                            <div class="flex flex-col space-y-3">
-                                <x-show-link href="{{ 'https://'.$link->url }}" target="_blank">
-                                    <p class="text-lg ">{{ $link->text }}</p>
-                                </x-show-link>        
-                            </div>
-                        </div>
-                    @endforeach
                 </div>
-            </div>
+            @endforeach
         </div>
-    </body>
-</html>
+    </div>
+
+</x-app-layout>
